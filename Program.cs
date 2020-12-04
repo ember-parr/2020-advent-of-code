315c615b6d2a118396f4be2cf822f45896a733d7
Ember Parr <ember.devops@gmail.com>
2020-12-03 21:18:55 -0600

    day one complete

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_of_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // DayOne dayOneCode = new DayOne();
+            // dayOneCode.PartTwo();
+
+        }
+    }
+}
